ba210f9657ffe95786293718172faa8de98b5928
pizzy <horlasehinde@gmail.com>
2021-04-30 11:49:50 +0100

    Add documentation for methods done so far.

--- cmc_api/coinmarketcap.py
+++ cmc_api/coinmarketcap.py
@@ -29,6 +29,7 @@ class CoinMarketCap:
 
     @staticmethod
     def _init_session(api_key):
+        """Initialize session which would be used for requests."""
         session = Session()
         headers = {
             'X-CMC_PRO_API_KEY': api_key,
@@ -39,6 +40,7 @@ class CoinMarketCap:
         return session
 
     def _insert_cat(self, text, cat, options=['crypto', 'exchange']):
+        """Insert cat into text if in options."""
         if cat in options:
             return text.format(self.categories[cat])
         else:
@@ -47,6 +49,7 @@ class CoinMarketCap:
                 "Valid options are: {{{}}}".format(cat, ','.join(options)))
 
     def _get_url(self, url, parameters={}):
+        """Get Response.json()['data']."""
         response = self.session.get(url, params=parameters)
         res = response.json()
         if response.status_code == 200:
@@ -79,20 +82,157 @@ class CoinMarketCap:
         pass
 
     def map(self, cat='crypto', **parameters):
+        """
+        Get ID map for cryptocurrency, exchange or fiat.
+
+        Parameters
+        ----------
+        cat: {'crypto', 'exchange', 'fiat'}, default 'crypto'
+            The category to get map for.
+        **parameters:
+            aux: default "platform,first_historical_data,last_historical_data,is_active"
+                all: platform,first_historical_data,last_historical_data,is_active,status
+                cat: {'crypto', 'exchange'}
+            crypto_id: str
+                cat: {'exchange'}
+            include_metals: bool, default False
+                cat: {'fiat'}
+            limit: int {1...5000}, optional
+                cat: {'crypto', 'exchange', 'fiat'}
+            listing_status: str or sequence of strs, default 'active'
+                option: {'active', 'inactive', 'untracked'}.
+                cat: {'crypto', 'exchange'}
+            slug: str or sequence of strs
+                cat: {'exchange'}
+            start: int, default 1
+                cat: {'crypto', 'exchange', fiat'}
+            sort: str, default 'id'
+                option: {'id', 'cmc_rank', 'name', 'volume_24h'}
+                cat: {'crypto', 'exchange', 'fiat'}
+            symbol: str or sequence of strs, optional
+                cat: {'crypto'}
+        
+        Returns
+        -------
+        data: dict
+
+        References
+        ----------
+        .. [1] `crypto map
+            <https://coinmarketcap.com/api/documentation/v1/#operation/getV1CryptocurrencyMap>`_
+        .. [2] `fiat map
+            <https://coinmarketcap.com/api/documentation/v1/#operation/getV1FiatMap>`_
+        .. [3] `exchange map
+            <https://coinmarketcap.com/api/documentation/v1/#operation/getV1ExchangeMap>`_
+        """
         url = self._insert_cat(self.BASE_URL + '/{}/map', cat,
               ['crypto', 'exchange', 'fiat'])
         return self._get_url(url, parameters)
 
     def listings(self, cat='crypto', **parameters):
+        """
+        Get latest listings for cryptocurrency or exchange.
+
+        Parameters
+        ----------
+        cat: {'crypto', 'exchange', 'fiat'}, default 'crypto'
+        **parameters:
+            start: int, default 1
+                cat: {'crypto', 'exchange'}
+            limit: int {1...5000}, default 100
+                cat: {'crypto', 'exchange'}
+            price_min: int or float {0...1e17}, optional
+                cat: {'crypto'}
+            price_max: int or float {0...1e17}, optional
+                cat: {'crypto'}
+            market_cap_min: int or float {0...1e17}, optional
+                cat: {'crypto'}
+            market_cap_max: int or float {0...1e17}, optional
+                cat: {'crypto'}
+            volume_24h_min: int or float {0...1e17}, optional
+                cat: {'crypto'}
+            volume_24h_max: int or float {0...1e17}, optional
+                cat: {'crypto'}
+            circulating_supply_min: int or float {0...1e17}, optional
+                cat: {'crypto'}
+            circulating_supply_max: int or float {0...1e17}, optional
+                cat: {'crypto'}
+            percent_change_24h_min: int or float {0...1e17}, optional
+                cat: {'crypto'}
+            percent_change_24h_max: int or float {0...1e17}, optional
+                cat: {'crypto'}
+            convert: str, optional
+                cat: {'crypto', 'exchange'}
+            convert_id: str, optional
+                cat: {'crypto', 'exchange'}
+            sort: str, default 'market_cap'
+                option: {'name', 'symbol', 'date_added', 'market_cap',
+                    'market_cap_by_total_supply_strict', 'volume_7d',
+                    'volume_30d', 'volume_24h',  'volume_24h_adjusted',
+                    'exchange_score'}
+                cat: {'crypto', 'exchange'}
+            sort_dir: {'asc', 'desc'}, optional
+                cat: {'crypto', 'exchange'}
+            cryptocurrency_type: {'all', 'coins', 'tokens'}, default 'all'
+                cat: {'crypto'}
+            tag: str, default 'all'
+                option: {'all', 'defi', 'filesharing'}
+                cat: {'crypto'}
+            market_type: str, default 'all'
+                option: {'fees','no_fees', 'all'}
+                cat: {'exchange'}
+            aux: str or sequence of str
+                cat: {'crypto', 'exchange'}
+
+        Returns
+        -------
+        data: dict
+
+        References
+        ----------
+        .. [1] `crypto listings
+            <https://coinmarketcap.com/api/documentation/v1/#operation/getV1CryptocurrencyListingsLatest>`_
+        .. [2] `exchange listings
+            <https://coinmarketcap.com/api/documentation/v1/#operation/getV1ExchangeListingsLatest>`_
+        """
         url = self._insert_cat(self.BASE_URL + '/{}/listings/latest', cat)
         return self._get_url(url, parameters)
 
     def info(self, cat='crypto', **parameters):
+        """
+        Get Metadata for cryptocurrency or exchange.
+
+        Parameters
+        ----------
+        cat: {'crypto', 'exchange', 'key'}, default 'crypto'
+        **parameters: id or slug or symbol is mandatory
+            id: str or sequence of strs
+                cat: {'crypto', 'exchange'}
+            slug: str or sequence of strs
+                cat: {'crypto', 'exchange'}
+            symbol: str or sequence of strs
+                cat: {'crypto'}
+            aux: str or sequence of str
+                cat: {'crypto', 'exchange'}
+        
+        Returns
+        -------
+        data: dict
+
+        References
+        ----------
+        .. [1] `crypto info
+            <https://coinmarketcap.com/api/documentation/v1/#operation/getV1CryptocurrencyInfo>`_
+        .. [2] `exchange info
+            <https://coinmarketcap.com/api/documentation/v1/#operation/getV1ExchangeInfo>`_
+        """
         url = self._insert_cat(self.BASE_URL + '/{}/info', cat, 
               ['crypto', 'exchange', 'key'])
         return self._get_url(url, parameters)
 
     def key_info(self):
+        """
+        Get API key details and usage stats.
+        """
         return self.info('key')
 
-    
